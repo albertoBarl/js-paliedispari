393310fd706317c69197634b3a256324ecbac75c
albertoBarl <alberto.barletta00@gmail.com>
2022-12-05 15:18:52 +0100

    problem discomposed

---
+++ Palindroma/js/script.js
@@ -0,0 +1,12 @@
+// Chiedere all’utente di inserire una parola e Creare una funzione per capire se la parola inserita è palindroma
+
+// 1- Inserire una parola
+// 2- Creare una funzione
+// 2.1- SE parola palindroma
+// 2.2 ALTRIMENTI non è una parola palindroma
+
+let word = prompt('Inserire una parola')
+
+function pal(){
+
+}
